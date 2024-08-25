e8451cf3d0503564eec8ef61e9d92e7ea03e8944
yossdeez <svataraska@gmail.com>
2024-08-25 19:03:52 +0200

    Better implementation(currently cant be tested)
    lmao2

--- RyanConnectionFinder/AvailabilitiesRequest.cs
+++ RyanConnectionFinder/AvailabilitiesRequest.cs
@@ -3,14 +3,34 @@
 public class AvailabilitiesRequest
 {
     
-    public static string[]? FlightDates(string lhs, string transfer, string rhs) 
-    { 
-       /* var airports = (lhs, transfer, rhs);
-        for (int i = 0; i < 2; i++) {
-            string url = "https://www.ryanair.com/api/farfnd/3/oneWayFares/{lhs}/{transfer}/availabilities"
-        }
-        */
+    public static string[]? FlightDates(string[] route)
+    {
+        /*
+        string[]? MergedDates = null; 
+        string urlbase = "https://www.ryanair.com/api/farfnd/3/oneWayFares/{0}/{1}/availabilities";
+        
+        for (int i = 0; i < route.Length-1; i++) 
+        {
+            string url = urlbase.Replace("{0}", route[i]).Replace("{1}", route[i + 1]);
+            var dates = NetworkClient.GetDataAsync<string[]>(url: url).Result;
 
+            if (i == 0)
+            {
+                MergedDates = dates;
+            }
+                
+            else
+            {
+                MergedDates = MergedDates.Intersect(dates).ToArray();
+                if (MergedDates.Length == 0)
+                {
+                    break;
+                }
+            }
+            
+        }
+        return MergedDates.ToArray();
+        */
         
         string url1 = "https://www.ryanair.com/api/farfnd/3/oneWayFares/{lhs}/{transfer}/availabilities";
         string url2 = "https://www.ryanair.com/api/farfnd/3/oneWayFares/{transfer}/{rhs}/availabilities";
